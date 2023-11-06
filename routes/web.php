0a83b189a23d16aeee13b9edf6f6358f7fe2d9eb
Darya Sadikova <sadidash@yandex.ru>
2023-11-06 18:16:25 +0300

    Lab1

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+
+/*
+|--------------------------------------------------------------------------
+@@ -13,6 +14,17 @@
+|
+*/
+
+// Route::get('/', function () {
+//     return view('main.main');
+// });
+
+Route::get('/', [MainController::class, 'index']);
+
+Route::get('contacts', function(){
+    $contact = [
+        'name'=>'Polytech',
+        'adress' => 'B.Semenovskaya 38',
+        'phone' => '8(495)232-3232'
+    ];
+    return view('main.contact', ['data' => $contact]);
+}); 

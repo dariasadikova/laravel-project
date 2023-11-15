fae5371e1e130d36e8e0321db3d40296859cc75b
Darya Sadikova <sadidash@yandex.ru>
2023-11-15 23:57:20 +0300

    Read - comment

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Comment;
 
 class Article extends Model
 {
@@ -14,4 +15,9 @@ class Article extends Model
         'desc',
         'authorId'
     ];
+
+
+    public function comment(){
+        return $this->hasMany(Comment::class);
+    }
 }

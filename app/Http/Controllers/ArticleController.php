51f2d648db36c5ccec2337b7b7931b56bd2b6c70
Darya Sadikova <sadidash@yandex.ru>
2023-11-26 00:45:04 +0300

    Queue

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,8 +5,7 @@ use App\Models\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
-use App\Mail\ArticleMail;
+use App\Jobs\ArticleMailJob;
 
 class ArticleController extends Controller
 {
@@ -49,7 +48,7 @@ class ArticleController extends Controller
         $article->desc = $request->desc;
         $article->author_id = 1;
         $article->save();
-        Mail::to('sadidash18@gmail.com')->send(new ArticleMail($article));
+        ArticleMailJob::dispatch($article);
         return redirect('/article');
     }
     /**

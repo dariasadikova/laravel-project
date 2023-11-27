e9685f74bda6596fa13bdb577bcc4b527dd924e9
Darya Sadikova <sadidash@yandex.ru>
2023-11-27 17:00:44 +0300

    notifications

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -33,6 +33,14 @@ class AuthServiceProvider extends ServiceProvider
             if($user->role === 'moderator') return true;
         });
 
+        
+        Gate::define('admincomment', function(User $user){
+            if ($user->role === 'moderator'){
+                return Response::allow();
+            } return Response::deny('В доступе отказано!');
+        });
+
+
 
         Gate::define('comment', function(User $user, Comment $comment){
             if ($user->id === $comment->author_id){

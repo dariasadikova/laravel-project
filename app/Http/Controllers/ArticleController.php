b94ca3b0bd7934da7b1dae11a6ec0a695c37a94c
Darya Sadikova <sadidash@yandex.ru>
2023-11-28 03:42:17 +0300

    cache

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,7 +5,9 @@ use App\Models\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
 use App\Jobs\ArticleMailJob;
 use App\Events\EventNewComment;
 
@@ -18,7 +20,10 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::latest()->paginate(5);
+        $page = isset($_GET['page']) ? $_GET['page']: 0;
+        $articles = Cache::remember('articles'.$page, 3000, function(){
+            return Article::latest()->paginate(5);
+        });
         return view('articles.main', ['articles'=>$articles]);
     }
     /**
@@ -49,10 +54,14 @@ class ArticleController extends Controller
         $article->short_desc = $request->shortDesc;
         $article->desc = $request->desc;
         $article->author_id = 1;
-        $article->save();
-         // Log::alert($article);
-        EventNewComment::dispatch($article);
-        ArticleMailJob::dispatch($article);
+        $res = $article->save();
+        if ($res){
+            ArticleMailJob::dispatch($article);
+            $keys = DB::table('cache')->whereRaw('`key` GLOB :key', [':key'=>'articles*[0-9]'])->get();
+            foreach($keys as $key){
+                Cache::forget($key->key);
+            }            
+        }
         return redirect('/article');
     }
     /**
@@ -63,15 +72,23 @@ class ArticleController extends Controller
      */
     public function show(Article $article)
     {
-        if (isset($_GET['notify'])){
-            auth()->user()->notifications->where('id', $_GET['notify'])->first()->markAsRead();
+        {
+            if (isset($_GET['notify'])){
+                auth()->user()->notifications->where('id', $_GET['notify'])->first()->markAsRead();
+            }
+            $page = isset($_GET['page']) ? ($_GET['page']) : 0;
+            $comments = Cache::rememberForever($article->id.'/comments'.$page,function()use($article){
+                return Comment::where('article_id', $article->id)
+                                ->where('accept', 1)
+                                ->latest()->paginate(2);
+            });
+    
+            return view('articles.show', ['article'=>$article, 'comments'=>$comments]);
         }
-
-        $comments = Comment::where('article_id', $article->id)
-        ->where('accept', 1)
-        ->latest()->paginate(2);
-        return view('articles.show', ['article'=>$article, 'comments'=>$comments]);
+    
+    
     }
+    
     /**
      * Show the form for editing the specified resource.
      *
@@ -102,7 +119,17 @@ class ArticleController extends Controller
         $article->short_desc = $request->shortDesc;
         $article->desc = $request->desc;
         $article->author_id = 1;
-        $article->save();
+        $res = $article->save();
+        if ($res){
+            $keys = DB::table('cache')->whereRaw('`key` GLOB :key', [':key'=>$article->id.'/comments*[0-9]'])->get();
+            foreach($keys as $key){
+                Cache::forget($key->key);
+            }
+            $keys = DB::table('cache')->whereRaw('`key` GLOB :key', [':key'=>'articles*[0-9]'])->get();
+            foreach($keys as $key){
+                Cache::forget($key->key);
+            }
+        }
         return redirect()->route('article.show', ['article'=>$article]);
     }
     /**
